feb1edc3855d84ceb33da79e21625d9c10f1b150
Your Name <you@example.com>
2023-08-27 12:33:49 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: The first integer to be computed
+ * @n2: The second integer to be computed
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}

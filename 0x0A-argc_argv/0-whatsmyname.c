151e920483429413447c4133df3e78dfc731a585
Your Name <you@example.com>
2023-09-06 02:01:48 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void) argc;
+	printf("%s\a", argv[0]);
+
+	return (0);
+
+}

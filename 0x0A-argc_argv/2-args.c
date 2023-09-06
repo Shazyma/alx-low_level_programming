e8d2e2398373f5e964eff99acc395d6f49ee7378
Your Name <you@example.com>
2023-09-06 02:56:11 -0700

    you can do anything

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: argument count
+ * 2argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+
+}

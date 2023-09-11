f46e7659c351ecc3f966a3b1948d510257138db5
Your Name <you@example.com>
2023-09-11 06:37:59 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+
+
+#define PI 3.14169265359
+
+
+
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -prints the name of the file
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+
+{
+	pintf("%s\n", _FILE_);
+	return (0);
+
+}

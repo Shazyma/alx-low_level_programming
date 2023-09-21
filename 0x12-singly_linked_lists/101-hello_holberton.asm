e3993f9694eff1d63c4f62d9208710fbd9f53e0f
Your Name <you@example.com>
2023-09-21 12:18:24 -0700

    hello singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,26 @@
-   exterm printf
+; File: 101-hello_holberton.asm
+; Auth: Richard Orido
+; Desc: 64-bit assembly program that prints
+; Hello, Holberton followed by a new line.
+
+extern printf
+
+section .text
    global main
 
 main:
    push rbp
 
    mov rdi,fmt
-   xor rax,rax
+   mov rsi,msg
+   mov rax,0
    call printf
+
+   pop rbp
+
    mov rax,0
    ret
 
-format: db "Hello, Holberton", 0 
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
